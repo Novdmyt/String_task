7a86b02af42c7e126a09e9b6252224e935aa4587
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-02 15:07:37 +0300

    Add method getData()

---
+++ src/app/task_2/Task_2StringBuffer.java
@@ -0,0 +1,31 @@
+package app.task_2;
+
+public class Task_2StringBuffer {
+    public static void main(String[] args) {
+
+        Task_2StringBuffer ts = new Task_2StringBuffer();
+        ts.getWord();
+    }
+
+    public   String world ="orange, plum, tomato, onion, grape, onion";
+
+       private String getData(){
+        StringBuffer sb = new StringBuffer(world);
+        int index;
+        for(int i = 0; i < sb.length(); i++){
+            if((index = sb.indexOf("onion", i)) == i){
+                sb.delete(index, index + "onion, ".length());
+            }
+        }
+        return sb.toString();
+    }
+    private void getWord(){
+        String[ ]  worl = getData().split(", ");
+        int count = 0;
+        for (String s: worl){
+            count++;
+            System.out.println(count + ") " + s);
+        }
+    }
+
+}
