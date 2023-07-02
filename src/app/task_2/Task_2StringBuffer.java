5f8a9b51a11b40565e03f9c8eb641c9c313de766
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-02 21:42:39 +0300

    Зміна метода getWord

--- src/app/task_2/Task_2StringBuffer.java
+++ src/app/task_2/Task_2StringBuffer.java
@@ -2,30 +2,35 @@ package app.task_2;
 
 public class Task_2StringBuffer {
     public static void main(String[] args) {
-
         Task_2StringBuffer ts = new Task_2StringBuffer();
-        ts.getWord();
+        String[] words = getData(); // Отримуємо масив слів
+        System.out.println(ts.getWord(words)); // Виводимо результат
     }
 
-    public   String world ="orange, plum, tomato, onion, grape, onion";
+    private static String world = "orange, plum, tomato, onion, grape, onion";
 
-       private String getData(){
+    private static String[] getData() {
         StringBuffer sb = new StringBuffer(world);
         int index;
-        for(int i = 0; i < sb.length(); i++){
-            if((index = sb.indexOf("onion", i)) == i){
-                sb.delete(index, index + "onion, ".length());
+        for (int i = 0; i < sb.length(); i++) {
+            if ((index = sb.indexOf("onion", i)) == i) {
+                sb.delete(index, index + "  onion".length());
             }
         }
-        return sb.toString();
-    }
-    private void getWord(){
-        String[ ]  word = getData().split(", ");
-        int count = 0;
-        for (String s: word){
-            count++;
-            System.out.println(count + ") " + s);
-        }
+        return sb.toString().split(", "); // Повертаємо масив слів
     }
 
+    private String getWord(String[] items) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int count = 0;
+        for (String s : items) {
+            count++;
+            stringBuffer
+                    .append(count)
+                    .append(") ")
+                    .append(s)
+                    .append("\n");
+        }
+        return stringBuffer.toString();
+    }
 }
