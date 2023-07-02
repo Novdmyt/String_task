1f552232280e52bbe60fd7abe35252ad2ca260b8
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-02 14:18:04 +0300

    Add method String getWorld() and getData()

---
+++ src/app/task_1/Task_1String.java
@@ -0,0 +1,24 @@
+package app.task_1;
+
+public class Task_1String {
+
+    public static void main(String[] args) {
+        Task_1String ts = new Task_1String();
+        ts.getData();
+    }
+    public String world = " brange, plum, tomato, onibn, grape";
+
+    private void getData(){
+        String[ ] world = getWorld().split(",");
+        int count = 0;
+        for(String s : world){
+            count++;
+            System.out.println(count + ") " + s);
+        }
+
+    }
+    private  String getWorld(){
+        world = world.replace("b","o");
+        return world;
+    }
+}
